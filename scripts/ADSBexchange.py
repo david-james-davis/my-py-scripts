8ebd359d8852f71bdcb0f70d40c2520e0538dd41
David Davis <davidjamesdavis.djd@gmail.com>
2025-03-14 14:37:33 -0700

    wip adsb script

--- scripts/ADSBexchange.py
+++ scripts/ADSBexchange.py
@@ -1,5 +1,51 @@
-def main():
-  print("aircraft curl soon")
+import os
+import socket
+import requests
+import time
+from dotenv import load_dotenv
 
-if __name__ == '__main__':
+load_dotenv()
+
+
+def main():
+    adsb_key = os.environ.get("ADSB_KEY")
+    target_ip_address = os.environ.get("TARGET_IP")
+    target_port = os.environ.get("TARGET_PORT")
+    target_lat = os.environ.get("TARGET_LAT")
+    target_lon = os.environ.get("TARGET_LON")
+    target_miles = os.environ.get("TARGET_MILES")
+    url = (
+        "https://adsbexchange-com1.p.rapidapi.com/v2/lat/"
+        + target_lat
+        + "/lon/"
+        + target_lon
+        + "/dist/"
+        + target_miles
+        + "/"
+    )
+    headers = {
+        "x-rapidapi-host": "adsbexchange-com1.p.rapidapi.com",
+        "x-rapidapi-key": adsb_key,
+    }
+
+    while True:
+        try:
+            response = requests.get(url, headers)
+            response.raise_for_status()
+            data = response.text
+        except Exception as e:
+            print("Error fetching data:" + e)
+
+        if data:
+            try:
+                with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                    s.connect((target_ip_address, target_port))
+                    s.sendall(data.encode("utf-8"))
+            except Exception as e:
+                print(f"Error sending data: {e}")
+
+        time.sleep(5)
+
+
+if __name__ == "__main__":
     main()
